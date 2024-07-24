7deff59e2b8932be8faaa8f84fcd2be682234fd2
Chanakya-dev <Chanakyamanas123@gmail.com>
2024-07-24 09:32:31 +0530

    Create Lists.java

---
+++ Lists.java
@@ -0,0 +1,14 @@
+public class Lists{
+public static void main(String[]args){
+// Create an Array List Object
+  // Add Elements
+  // Iterate them using Iterator or List Iterator
+// Create a Linked List Object
+  // Add Elements
+  // Iterate them using Iterator or List Iterator
+  // Create a Stack Object
+  // Add Elements
+  // Iterate them using Enumaration
+
+}
+}
